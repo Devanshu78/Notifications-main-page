6e72af4d36f299062f3a31a3c57741c589cde2bd
Devanshu <devanshupandey281@gmail.com>
2023-08-20 00:14:05 +0530

    first push

---
+++ script.js
@@ -0,0 +1,10 @@
+const markAsDone = document.querySelector('.toggelMessageStatus');
+const unreadMessageNumber = document.querySelector('.unreadMessageNumber');
+const unreadMessage = document.querySelectorAll('.messageBg');
+
+markAsDone.addEventListener('click' , function(){
+    unreadMessageNumber.classList.add('hidden');
+    unreadMessage.forEach((unreadMessage) =>{
+        unreadMessage.classList.add('messageRead');
+    });
+})
